613e33b08eedbdd1c9530ded57294b956272738a
arunjoseexpedia <91180670+arunjoseexpedia@users.noreply.github.com>
2022-03-21 16:46:11 +0530

    page Title page

--- src/AddStory.js
+++ src/AddStory.js
@@ -1,16 +1,23 @@
 export default function AddStory() {
   return (
     <div className="container-fluid">
-      <form>
-        <label>
-          Page Title
-          <input type="text" required />
-        </label>
-        <label>
-          Essay:
-          <textarea> </textarea>
-        </label>
-        <input type="submit" value="Submit" />
+      <form align="center">
+        <div>
+          <label>
+            Page Title
+            <br />
+            <input className="pagetitle" type="text" required />
+          </label>
+        </div>
+        <div>
+          <label>
+            <textarea> </textarea>
+          </label>
+        </div>
+        <div>
+          <button type="submit">Save</button>
+          <button type="submit">Back</button>
+        </div>
       </form>
     </div>
   );
