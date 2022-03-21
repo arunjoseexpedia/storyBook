1a710332aa1bf9d969c9021dada6897eb87fea39
arunjoseexpedia <91180670+arunjoseexpedia@users.noreply.github.com>
2022-03-21 15:03:40 +0530

    text area

--- src/AddStory.js
+++ src/AddStory.js
@@ -1,3 +1,17 @@
 export default function AddStory() {
-  return <div className="container-fluid">add story</div>;
+  return (
+    <div className="container-fluid">
+      <form>
+        <label>
+          Page Title
+          <input type="text" required />
+        </label>
+        <label>
+          Essay:
+          <textarea> </textarea>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
