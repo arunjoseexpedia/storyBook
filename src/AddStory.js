9b199e907b68dd12e413dd94e20153331f88537c
arunjoseexpedia <91180670+arunjoseexpedia@users.noreply.github.com>
2022-03-22 17:04:11 +0530

    wordcount default value

--- src/AddStory.js
+++ src/AddStory.js
@@ -54,7 +54,7 @@ export default function AddStory(props) {
               {" "}
             </textarea>
           </label>
-          {wordCount}
+          {wordCount > 0 ? wordCount : ""}
         </div>
         <div>
           <Button className="backViolet" type="submit">
